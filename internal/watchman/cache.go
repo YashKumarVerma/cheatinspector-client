45f80b9825c4be79525f077367a14f26ed3a4b89
Yash Kumar Verma <yk.verma2000@gmail.com>
2021-05-06 13:26:17 +0530

    feat(cache): encoding and decoding

--- internal/watchman/cache.go
+++ internal/watchman/cache.go
@@ -1,19 +1,40 @@
 package watchman
 
 import (
+	"bytes"
+	"encoding/gob"
 	"fmt"
+	"github.com/YashKumarVerma/hentry-client/internal/fs"
 	"github.com/allegro/bigcache/v3"
 	"time"
 )
 
+
+var cache *bigcache.BigCache
+var network bytes.Buffer
+var encoder *gob.Encoder
+var decoder *gob.Decoder
+
+// Init to setup and initialize cache handlers
 func Init() {
-	cache,_ = bigcache.NewBigCache(bigcache.DefaultConfig(10 * time.Minute))
+	// initialize cache handler
+	cache, _ = bigcache.NewBigCache(bigcache.DefaultConfig(10 * time.Minute))
 	cache.Set("status", []byte("Cache Serve Alive"))
 	entry, _ := cache.Get("status")
 	fmt.Println(string(entry))
 
+	// initialise encoders and decoders
+	encoder = gob.NewEncoder(&network)
+	decoder = gob.NewDecoder(&network)
 }
 
+// setCache saves the key and vale into storage
+func setCache(fileDetails fs.FileDetails) bool {
+	err := encoder.Encode(fileDetails)
+	if err != nil {
+		fmt.Println("Error encoding value : " , fileDetails)
+		cache.Set(fileDetails.Path, network.Bytes())
+	}
+	//cache.Set(index, []byte(fileDetails))
+}
 
-
-var cache *bigcache.BigCache
--- internal/watchman/struct.go
+++ internal/watchman/struct.go
@@ -1,11 +1,2 @@
 package watchman
 
-// Node represents a file on disk, with few attributes that assist in tracking changes
-type Node struct {
-	filename     string
-	absolutePath string
-	lastEdited   int64
-	lastParsed   int64
-	previousSize int
-	
-}
