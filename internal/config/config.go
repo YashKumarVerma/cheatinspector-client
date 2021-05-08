22c6f49d565775e2ec742280516574e97d56efb4
Yash Kumar Verma <yk.verma2000@gmail.com>
2021-05-09 01:43:20 +0530

    fix(app): load feeder configs

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,14 +5,15 @@ func Init() {
 	data := ConfigStruct{
 		Name:      "hentry",
 		Server:    "http://localhost:8000",
-        Feeder:    "http://localhost:9898",
-        Frequency: 10,
+		Feeder:    "http://localhost:9898",
+		Frequency: 10,
 		FileName:  ".hentryrc",
 	}
 
 	Load.Name = data.Name
 	Load.Server = data.Server
 	Load.Frequency = data.Frequency
+	Load.Feeder = data.Feeder
 }
 
 var Load ConfigStruct
