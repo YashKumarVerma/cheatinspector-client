0e044f27ca25c3876e76f2ee9f102c3b3fcc0dd0
Yash Kumar Verma <yk.verma2000@gmail.com>
2021-05-10 00:44:06 +0530

    feat(config): load -api=XXX -feed=XXX from CLI

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,17 +1,23 @@
 package config
 
+import "flag"
+
 // Load returns the current configurations
 func Init() {
+
+	serverURL := flag.String("api", "https://hentry.uniauth.co", "URL of main API server")
+	feeder := flag.String("feed", "https://hentry.uniauth.co", "URL of main API server")
+
 	data := ConfigStruct{
 		Name:      "hentry",
-		Server:    "http://localhost:8000",
-		Feeder:    "http://localhost:9898",
+		Server:    *serverURL,
+		Feeder:    *feeder,
 		Frequency: 10,
 		FileName:  ".hentryrc",
 	}
 
 	Load.Name = data.Name
-	Load.Server = data.Server
+	Load.Server = data.Feeder
 	Load.Frequency = data.Frequency
 	Load.Feeder = data.Feeder
 }
