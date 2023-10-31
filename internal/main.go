db0e7f4653c07514f0673b8cfa7e96ae20bc2d1f
shubhamji88 <shubham.shankaram2019@vitstudent.ac.in>
2023-10-31 20:49:40 +0530

    temp changes for direct working

--- internal/main.go
+++ internal/main.go
@@ -4,13 +4,15 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/YashKumarVerma/hentry-client/internal/ably"
 	"github.com/YashKumarVerma/hentry-client/internal/config"
-	"github.com/YashKumarVerma/hentry-client/internal/device"
+
+	// "github.com/YashKumarVerma/hentry-client/internal/device"
 	"github.com/YashKumarVerma/hentry-client/internal/fs"
 	"github.com/YashKumarVerma/hentry-client/internal/sensor"
-	"github.com/YashKumarVerma/hentry-client/internal/team"
+
+	// "github.com/YashKumarVerma/hentry-client/internal/team"
 	"github.com/YashKumarVerma/hentry-client/internal/watchman"
-	"github.com/YashKumarVerma/hentry-client/internal/ably"
 )
 
 func main() {
@@ -19,53 +21,53 @@ func main() {
 	sensor.Init()
 	watchman.Init()
 	ably.Init()
-	
+
 	// holders for user data
-	var UserTeam team.Team
-	var UserDevice device.Device
+	// var UserTeam team.Team
+	// var UserDevice device.Device
 
-	//register or join a team
-	teamOperation := team.CreateOrJoinTeamScreen()
-	if teamOperation == "create" {
-		teamDetails := team.CreateTeamScreen()
-		teamAPIResponse := team.CreateTeamAPI(teamDetails)
-		UserTeam = teamAPIResponse
-	} else {
-		teamDetails := team.JoinTeamScreen()
-		teamAPIResponse := team.GetTeamDetailAPI(teamDetails.TeamID)
-		UserTeam = teamAPIResponse
-	}
+	// //register or join a team
+	// teamOperation := team.CreateOrJoinTeamScreen()
+	// if teamOperation == "create" {
+	// 	teamDetails := team.CreateTeamScreen()
+	// 	teamAPIResponse := team.CreateTeamAPI(teamDetails)
+	// 	UserTeam = teamAPIResponse
+	// } else {
+	// 	teamDetails := team.JoinTeamScreen()
+	// 	teamAPIResponse := team.GetTeamDetailAPI(teamDetails.TeamID)
+	// 	UserTeam = teamAPIResponse
+	// }
 
-	// load all sensor data
-	userDeviceInfo := sensor.Load
-	notFound, deviceInfo := device.GetDeviceDetailAPI(userDeviceInfo.MachineID)
+	// // load all sensor data
+	// userDeviceInfo := sensor.Load
+	// notFound, deviceInfo := device.GetDeviceDetailAPI(userDeviceInfo.MachineID)
 
-	//  if not found, then initiate login process
-	if notFound == true {
-		fmt.Println("\nDevice not registered on Hentry, registration in process...")
-		deviceDetails := device.CreateTeamScreen()
-		// teamOperation := team.CreateOrJoinTeamScreen()
-		// if teamOperation == "create" {
-		// 	teamDetails := team.CreateTeamScreen()
-		// 	teamAPIResponse := team.CreateTeamAPI(teamDetails)
-		// 	UserTeam = teamAPIResponse
-		// } else {
-		// 	teamDetails := team.JoinTeamScreen()
-		// 	teamAPIResponse := team.GetTeamDetailAPI(teamDetails.TeamID)
-		// 	UserTeam = teamAPIResponse
-		// }
-		teamNotFound, deviceAPIResponse := device.RegisterDeviceAPI(deviceDetails, UserTeam.ID)
+	// //  if not found, then initiate login process
+	// if notFound == true {
+	// 	fmt.Println("\nDevice not registered on Hentry, registration in process...")
+	// 	deviceDetails := device.CreateTeamScreen()
+	// 	// teamOperation := team.CreateOrJoinTeamScreen()
+	// 	// if teamOperation == "create" {
+	// 	// 	teamDetails := team.CreateTeamScreen()
+	// 	// 	teamAPIResponse := team.CreateTeamAPI(teamDetails)
+	// 	// 	UserTeam = teamAPIResponse
+	// 	// } else {
+	// 	// 	teamDetails := team.JoinTeamScreen()
+	// 	// 	teamAPIResponse := team.GetTeamDetailAPI(teamDetails.TeamID)
+	// 	// 	UserTeam = teamAPIResponse
+	// 	// }
+	// 	teamNotFound, deviceAPIResponse := device.RegisterDeviceAPI(deviceDetails, UserTeam.ID)
 
-		if teamNotFound == true {
-			fmt.Println("Team does not exist")
-		} else {
-			fmt.Println("Device registered.")
-			UserDevice = deviceAPIResponse
-		}
-	} else {
-		fmt.Println("\nDevice already registered.")
-		UserDevice = deviceInfo
-	}
+	// 	if teamNotFound == true {
+	// 		fmt.Println("Team does not exist")
+	// 	} else {
+	// 		fmt.Println("Device registered.")
+	// 		UserDevice = deviceAPIResponse
+	// 	}
+	// } else {
+	// 	fmt.Println("\nDevice already registered.")
+	// 	UserDevice = deviceInfo
+	// }
 
 	success, folderNames := fs.ListFolders("./")
 	if !success {
@@ -89,5 +91,5 @@ func main() {
 		watchman.ResetTotal()
 	}
 
-	_ = UserDevice
+	// _ = UserDevice
 }
